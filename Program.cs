6696c855211034dd290c2b804ac61ba2f9bf6030
sergeylebedev <lebedevs06@gmail.com>
2022-09-05 20:35:54 +0400

    Большее из трёх

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿  Console.WriteLine("Введите три числа: ");
+  int nam_1 = Convert.ToInt32(Console.ReadLine());
+  int nam_2 = Convert.ToInt32(Console.ReadLine());
+  int nam_3 = Convert.ToInt32(Console.ReadLine());
+
+  int max = nam_1;
+
+  if (nam_2 > max)
+  {
+     max = nam_2;
+  }
+  if (nam_3 > max)
+  {
+    max = nam_3;
+  }
+  Console.WriteLine("Большее из трёх чисел: " + max);
+  
